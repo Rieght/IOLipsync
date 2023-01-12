f058281ccdb878c657d49fddf93ddfddff26b9f6
Rieght <33426181+Rieght@users.noreply.github.com>
2023-01-12 21:08:37 +0100

    Added eye movement. Reset mouth and eye movement after each line. Some refactoring.

--- IOLipsync/Hook.cs
+++ IOLipsync/Hook.cs
@@ -5,10 +5,11 @@ using HarmonyLib;
 using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace IOLipsync
 {
-    [BepInEx.BepInPlugin(_guid, "IOLipsync", "1.0.0")]
+    [BepInEx.BepInPlugin(_guid, "IOLipsync", "1.2.0")]
     public class Hook : BaseUnityPlugin
     {
         private const string _guid = "rieght.insultorder.iolipsync";
@@ -18,7 +19,7 @@ namespace IOLipsync
         public Hook()
         {
             var harmony = new Harmony(_guid);
-            harmony.PatchAll(typeof(TestHook));
+            harmony.PatchAll(typeof(DebugHook));
             harmony.PatchAll(typeof(UpdateBlendShapeHook));
 
             this.AddConfigs();
@@ -27,8 +28,10 @@ namespace IOLipsync
         public void AddConfigs()
         {
             LipsyncConfig.DebugMenu = Config.Bind<bool>(new ConfigDefinition("Debug", "Debug Menu"), false);
-            LipsyncConfig.MouthOpenness = Config.Bind<float>(new ConfigDefinition("Debug", "Mouth Openness"), 200f);
-            LipsyncConfig.MouthSmoothness = Config.Bind<float>(new ConfigDefinition("Debug", "Mouth Smoothness"), 20f);
+            LipsyncConfig.MouthOpenness = Config.Bind<float>(new ConfigDefinition("Mouth Movement", "Mouth Openness"), 200f);
+            LipsyncConfig.MouthSmoothness = Config.Bind<float>(new ConfigDefinition("Mouth Movement", "Mouth Smoothness"), 20f); LipsyncConfig.MouthOpenness = Config.Bind<float>(new ConfigDefinition("Mouth", "Mouth Openness"), 200f);
+            LipsyncConfig.EyeOpenness = Config.Bind<float>(new ConfigDefinition("Eye Movement", "Eye Openness"), 200f); LipsyncConfig.MouthOpenness = Config.Bind<float>(new ConfigDefinition("Mouth", "Mouth Openness"), 200f);
+            LipsyncConfig.EyeSmoothness = Config.Bind<float>(new ConfigDefinition("Eye Movement", "Eye Smoothness"), 10f);
         }
     }
 
@@ -37,6 +40,8 @@ namespace IOLipsync
         public ConfigEntry<bool> DebugMenu { get; set; }
         public ConfigEntry<float> MouthOpenness { get; set; }
         public ConfigEntry<float> MouthSmoothness { get; set; }
+        public ConfigEntry<float> EyeOpenness { get; set; }
+        public ConfigEntry<float> EyeSmoothness { get; set; }
     }
 
     public static class LipsyncDebug
@@ -46,40 +51,66 @@ namespace IOLipsync
 
     public static class UpdateBlendShapeHook
     {
-        private static bool _inDialogue;
-
         private static AudioSource _audioSource;
         private static Animator _animator;
 
         // Value must be a power of 2. Min = 64. Max = 8192.
         private static readonly int _numSamples = 1024;
         private static float[] _samples = new float[_numSamples];
-        private static float _sum;
-        private static float _openness;
+        private static float _volume = 0f;
+
+        private static float _eyeVolume = 0f;
+        private static float _eyeOpenness = 1f;
+
+        private static float _mouthVolume = 0f;
+        private static float _mouthOpenness = 0f;
+
 
         private static readonly string _nekoPath = "CH01/CH0001/HS_kiten/bip01/bip01 Pelvis/bip01 Spine/bip01 Spine1/bip01 Spine2/bip01 Neck/bip01 Head/HS_HeadScale/HS_Head";
         private static readonly string _tomoePath = "CH02/CH0002/HS_kiten/bip01/bip01 Pelvis/bip01 Spine/bip01 Spine1/bip01 Spine2/bip01 Neck/bip01 Head/HS_HeadScale/HS_Head";
 
-        // Update mouth openness
+        // Update mouth and eye openness
         [HarmonyPatch(typeof(FaceMotion), "Update")]
         [HarmonyPostfix]
         public static void PostUpdate()
         {
-            if (!_inDialogue) return;
-            if (_audioSource == null || !_audioSource.isPlaying || _audioSource.time > _audioSource.clip.length) return;
+            if (SceneManager.GetActiveScene().name != "FH") return;
+            if (_audioSource == null || !_audioSource.isPlaying || _audioSource.time > _audioSource.clip.length)
+            {
+                if (_animator == null) return;
 
+                // Reset mouth and eyes at the end of each line.
+                _animator.SetLayerWeight(2, 1f);
+                _animator.SetLayerWeight(7, 0f);
+                return;
+            }
+
+            // Calc volume
             _audioSource.GetOutputData(_samples, 0);
-            _sum = 0f;
+            _volume = 0f;
             for (int i = 0; i < _numSamples; i++)
             {
-                _sum = _samples[i] * _samples[i];
+                _volume = _samples[i] * _samples[i];
             }
-            _sum  = Mathf.Sqrt(_sum / _numSamples);
-            _openness = Mathf.SmoothStep(_openness, _sum, Time.deltaTime * Hook.LipsyncConfig.MouthSmoothness.Value);
+            _volume = Mathf.Sqrt(_volume / _numSamples);
 
-            // TODO: Maybe add some movement to the face (layer 2, default 1f)
+            // Calc eye openness
+            _eyeVolume = _volume * Hook.LipsyncConfig.EyeOpenness.Value;
+            if (_eyeVolume < 0.5f)
+                _eyeOpenness = Mathf.SmoothStep(_eyeOpenness, 1f, Time.deltaTime * Hook.LipsyncConfig.EyeSmoothness.Value);
+            else
+                _eyeOpenness = Mathf.SmoothStep(_eyeOpenness, 1f - _eyeVolume, Time.deltaTime * Hook.LipsyncConfig.EyeSmoothness.Value);
+            _eyeOpenness = Mathf.Clamp01(_eyeOpenness);
 
-            _animator.SetLayerWeight(7, Mathf.Clamp01(_openness * Hook.LipsyncConfig.MouthOpenness.Value));
+            // Calc mouth openness
+            _mouthVolume = _volume * Hook.LipsyncConfig.MouthOpenness.Value;
+            _mouthOpenness = Mathf.SmoothStep(_mouthOpenness, _mouthVolume, Time.deltaTime * Hook.LipsyncConfig.MouthSmoothness.Value);
+            _mouthOpenness = Mathf.Clamp01(_mouthOpenness);
+
+            // Change eyes
+            _animator.SetLayerWeight(2, _eyeOpenness);
+            // Change mouth
+            _animator.SetLayerWeight(7, _mouthOpenness);
         }
 
         // Get animator and audiosource when Neko/Tomoe says a new line.
@@ -88,8 +119,6 @@ namespace IOLipsync
         public static void PostWordPlay(int No)
         {
             // TODO: Don't update animator on every line.
-            _inDialogue = true;
-
             GameObject gameObjectWithAudioSources;
 
             var neko = GameObject.Find("CH0001");
@@ -116,7 +145,7 @@ namespace IOLipsync
 
     // Creates a Debugging Window for the control of animation layers.
     // Code by lynzrand https://github.com/lynzrand/kk-lipsync/blob/master/IOLipSync/Hook.cs
-    public static class TestHook
+    public static class DebugHook
     {
         private struct EnabledToggleKey : IEquatable<EnabledToggleKey>
         {
